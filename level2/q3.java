8ad248653eb18abc1e8d9177fe83ac75ffdec0ff
pc6818 <pc6818@srmist.edu.in>
2026-03-16 06:38:38 +0530

    Completed Level 2 Array practice programs

---
+++ level2/q3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class q3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        
+        int maxDigit = 10;
+        int[] digits = new int[maxDigit];
+        int index = 0;
+
+        while (number != 0) {
+            digits[index] = number % 10;
+            number /= 10;
+            index++;
+            if (index == maxDigit) break;
+        }
+
+        int largest = 0;
+        int secondLargest = 0;
+
+        for (int i = 0; i < index; i++) {
+            if (digits[i] > largest) {
+                secondLargest = largest;
+                largest = digits[i];
+            } else if (digits[i] > secondLargest && digits[i] != largest) {
+                secondLargest = digits[i];
+            }
+        }
+
+        System.out.println("Largest digit: " + largest);
+        System.out.println("Second largest digit: " + secondLargest);
+        input.close();
+    }
+}
